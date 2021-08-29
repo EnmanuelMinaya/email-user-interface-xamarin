2871ba9be07ebbf65f35372687bb33dc5ae85f47
Enmanuel Minaya <enmanuelminaya@gmail.com>
2021-08-28 23:59:50 -0400

    Added MVVM folders

---
+++ EmailUserInterface/EmailUserInterface/Models/Email.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmailUserInterface.Models
+{
+    public class Email
+    {
+        public string Sender { get; set; }
+        public string Receiver { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+
+    }
+}
---
+++ EmailUserInterface/EmailUserInterface/ViewModels/InboxViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace EmailUserInterface.ViewModels
+{
+    public class InboxViewModel : BaseViewModel
+    {
+
+
+
+        public ICommand GoToNewMessageCommand { get; }
+
+        public InboxViewModel()
+        {
+
+        }
+    }
+}
